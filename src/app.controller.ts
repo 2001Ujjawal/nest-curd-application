74eb0c137b11f20fd836dd2b4ed408ec1f6237ba
Ujjawal Sharma <ujjawalkpa09@gmail.com>
2026-04-23 11:42:22 +0530

    created first post route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post('/test-post')
+  checkPostRequest(): object {
+    console.log('hello this is post routes');
+    return {
+      success: 'hello',
+    };
   }
 }
