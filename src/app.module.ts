93a96870aef4998296010dec748082de4ecc87e0
2001Ujjawal <ujjawalkpa09@gmail.com>
2026-04-27 15:38:00 +0530

    added db connection and migration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest_curd_application',
+      autoLoadEntities: true,
+      synchronize: false, // dev only
+      migrations: ['dist/migrations/*.js'],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
