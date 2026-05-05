4b1084b9d74a6ff689518540dc9d48df183177e9
2001Ujjawal <ujjawalkpa09@gmail.com>
2026-05-05 11:11:49 +0530

    code update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from './common/interceptors/response/response.interceptor';
-import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+// import { ResponseInterceptor } from './common/interceptors/response/response.interceptor';
+// import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalInterceptors(new ResponseInterceptor());
+  // app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
