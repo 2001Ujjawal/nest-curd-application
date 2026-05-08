7fdf29ac6db90b8f10e4feaa936736e0d606639b
2001Ujjawal <ujjawalkpa09@gmail.com>
2026-05-08 18:31:35 +0530

    file upload

--- src/chat/chat_file.gateway.ts
+++ src/chat/chat_file.gateway.ts
@@ -1,12 +1,15 @@
 import {
+  MessageBody,
   SubscribeMessage,
   WebSocketGateway,
-  MessageBody,
-  OnGatewayDisconnect,
-  OnGatewayConnection,
   WebSocketServer,
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
+
 import { Server, Socket } from 'socket.io';
+
 @WebSocketGateway({
   cors: {
     origin: '*',
@@ -15,8 +18,10 @@ import { Server, Socket } from 'socket.io';
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+
   handleConnection(client: Socket) {
     console.log('User Connected:', client.id);
+    console.log('=============socket', Socket);
 
     // this.server.emit('message', {
     //   user: 'System',
@@ -31,12 +36,13 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     //   text: `User left`,
     // });
   }
-  @SubscribeMessage('upload')
-  handleUpload(@MessageBody() file: any) {
-    console.log(file);
-    return {
-      status: true,
-      message: 'File received successfully',
-    };
+
+  @SubscribeMessage('sendMessage')
+  handleMessage(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    console.log(data);
+    this.server.emit('message', {
+      user: data.user,
+      text: data.message,
+    });
   }
 }
